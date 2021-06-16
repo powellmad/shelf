ab7fc4bb12859f5e13949b7112cf7344ae99f507
powellmad <madsnpowell@gmail.com>
2021-06-15 20:12:45 -0500

    created fixtures to seed database and created auth viewset

--- shelf/urls.py
+++ shelf/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from shelfapi.views import register_user, login_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
