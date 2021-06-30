9d198a1efc1143ce875d558d6ebd5bf5bfa3804b
powellmad <madsnpowell@gmail.com>
2021-06-29 20:39:36 -0500

    added url to settings. py CORS

--- shelf/settings.py
+++ shelf/settings.py
@@ -57,7 +57,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://shelf-sm.netlify.app/'
 )
 
 MIDDLEWARE = [
